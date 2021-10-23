96f8b7093f9cb055ed535ab2af364c8d70540d58
DN-Vanguard <Domiltannguyen@outlook.com>
2021-10-23 06:22:08 -0700

    on error, upgrade, and success completed.

--- public/db.js
+++ public/db.js
@@ -3,3 +3,34 @@ let dbVersion = 3;
 const storeName = 'BudgetStore';
 
 const request = indexedDB.open( 'BudgetDB', dbVersion );
+
+request.onerror = function ( e ) {
+	console.log( `An error occured: ${e.target.errorCode}` );
+};
+
+request.onupgradeneeded = function ( e ) {
+	console.log( 'Upgrading indexedDB' );
+
+	const { oldVersion } = e;
+	const newVersion = e.newVersion || db.version;
+
+	console.log( `indexedDB updated from V${oldVersion} to V${newVersion}` );
+
+	db = e.target.result;
+
+	if( db.objectStoreNames.length === 0 ) {
+		db.createObjectStore( storeName, { autoIncrement: true } );
+	}
+};
+
+request.onsuccess = function ( e ) {
+	console.log( 'Successfully opened indexedDB' );
+
+	db = e.target.result;
+
+	if( navigator.onLine ) {
+		console.log( 'Backend online' );
+
+		checkDatabase();
+	}
+};
